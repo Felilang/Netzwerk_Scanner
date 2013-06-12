cff99a98665e09535dd491e6e0b3c7be7cf07f22
Felix Langner <Felix@langner-bzw.de>
2013-06-12 16:01:31 +0200

    commit

---
+++ Online Geräte suchen/Form1.Designer.vb
@@ -0,0 +1,411 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.IPQ1Box = New System.Windows.Forms.TextBox()
+        Me.IPQ2Box = New System.Windows.Forms.TextBox()
+        Me.IPQ3Box = New System.Windows.Forms.TextBox()
+        Me.IPQ4Box = New System.Windows.Forms.TextBox()
+        Me.IPQ5Box = New System.Windows.Forms.TextBox()
+        Me.IPQ6Box = New System.Windows.Forms.TextBox()
+        Me.IPQ7Box = New System.Windows.Forms.TextBox()
+        Me.IPQ8Box = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.DateiToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ExportierenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.ImportierenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.StartenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.BeendenToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.InfoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.VersionToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.HerstellerToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Label7 = New System.Windows.Forms.Label()
+        Me.Label8 = New System.Windows.Forms.Label()
+        Me.TreeView1 = New System.Windows.Forms.TreeView()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Label9 = New System.Windows.Forms.Label()
+        Me.ProgressBar1 = New System.Windows.Forms.ProgressBar()
+        Me.Label10 = New System.Windows.Forms.Label()
+        Me.Label11 = New System.Windows.Forms.Label()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Button1.Location = New System.Drawing.Point(9, 562)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(104, 35)
+        Me.Button1.TabIndex = 10
+        Me.Button1.Text = "Starten"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'IPQ1Box
+        '
+        Me.IPQ1Box.ImeMode = System.Windows.Forms.ImeMode.Off
+        Me.IPQ1Box.Location = New System.Drawing.Point(9, 55)
+        Me.IPQ1Box.MaxLength = 3
+        Me.IPQ1Box.Name = "IPQ1Box"
+        Me.IPQ1Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ1Box.TabIndex = 1
+        '
+        'IPQ2Box
+        '
+        Me.IPQ2Box.Location = New System.Drawing.Point(66, 55)
+        Me.IPQ2Box.MaxLength = 3
+        Me.IPQ2Box.Name = "IPQ2Box"
+        Me.IPQ2Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ2Box.TabIndex = 2
+        '
+        'IPQ3Box
+        '
+        Me.IPQ3Box.Location = New System.Drawing.Point(123, 55)
+        Me.IPQ3Box.MaxLength = 3
+        Me.IPQ3Box.Name = "IPQ3Box"
+        Me.IPQ3Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ3Box.TabIndex = 3
+        '
+        'IPQ4Box
+        '
+        Me.IPQ4Box.Location = New System.Drawing.Point(180, 55)
+        Me.IPQ4Box.MaxLength = 3
+        Me.IPQ4Box.Name = "IPQ4Box"
+        Me.IPQ4Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ4Box.TabIndex = 4
+        '
+        'IPQ5Box
+        '
+        Me.IPQ5Box.Location = New System.Drawing.Point(9, 110)
+        Me.IPQ5Box.MaxLength = 3
+        Me.IPQ5Box.Name = "IPQ5Box"
+        Me.IPQ5Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ5Box.TabIndex = 5
+        '
+        'IPQ6Box
+        '
+        Me.IPQ6Box.Location = New System.Drawing.Point(66, 110)
+        Me.IPQ6Box.MaxLength = 3
+        Me.IPQ6Box.Name = "IPQ6Box"
+        Me.IPQ6Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ6Box.TabIndex = 6
+        '
+        'IPQ7Box
+        '
+        Me.IPQ7Box.Location = New System.Drawing.Point(123, 110)
+        Me.IPQ7Box.MaxLength = 3
+        Me.IPQ7Box.Name = "IPQ7Box"
+        Me.IPQ7Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ7Box.TabIndex = 7
+        '
+        'IPQ8Box
+        '
+        Me.IPQ8Box.Location = New System.Drawing.Point(180, 110)
+        Me.IPQ8Box.MaxLength = 3
+        Me.IPQ8Box.Name = "IPQ8Box"
+        Me.IPQ8Box.Size = New System.Drawing.Size(51, 20)
+        Me.IPQ8Box.TabIndex = 8
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(9, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(42, 13)
+        Me.Label1.TabIndex = 9
+        Me.Label1.Text = "Start IP"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(9, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(39, 13)
+        Me.Label2.TabIndex = 10
+        Me.Label2.Text = "End IP"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.DateiToolStripMenuItem, Me.InfoToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(390, 24)
+        Me.MenuStrip1.TabIndex = 14
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'DateiToolStripMenuItem
+        '
+        Me.DateiToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ExportierenToolStripMenuItem, Me.ImportierenToolStripMenuItem, Me.StartenToolStripMenuItem, Me.BeendenToolStripMenuItem})
+        Me.DateiToolStripMenuItem.Name = "DateiToolStripMenuItem"
+        Me.DateiToolStripMenuItem.Size = New System.Drawing.Size(46, 20)
+        Me.DateiToolStripMenuItem.Text = "Datei"
+        '
+        'ExportierenToolStripMenuItem
+        '
+        Me.ExportierenToolStripMenuItem.Name = "ExportierenToolStripMenuItem"
+        Me.ExportierenToolStripMenuItem.Size = New System.Drawing.Size(136, 22)
+        Me.ExportierenToolStripMenuItem.Text = "Exportieren"
+        '
+        'ImportierenToolStripMenuItem
+        '
+        Me.ImportierenToolStripMenuItem.Name = "ImportierenToolStripMenuItem"
+        Me.ImportierenToolStripMenuItem.Size = New System.Drawing.Size(136, 22)
+        Me.ImportierenToolStripMenuItem.Text = "Importieren"
+        '
+        'StartenToolStripMenuItem
+        '
+        Me.StartenToolStripMenuItem.Name = "StartenToolStripMenuItem"
+        Me.StartenToolStripMenuItem.Size = New System.Drawing.Size(136, 22)
+        Me.StartenToolStripMenuItem.Text = "Starten"
+        '
+        'BeendenToolStripMenuItem
+        '
+        Me.BeendenToolStripMenuItem.Name = "BeendenToolStripMenuItem"
+        Me.BeendenToolStripMenuItem.Size = New System.Drawing.Size(136, 22)
+        Me.BeendenToolStripMenuItem.Text = "Beenden"
+        '
+        'InfoToolStripMenuItem
+        '
+        Me.InfoToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.VersionToolStripMenuItem, Me.HerstellerToolStripMenuItem})
+        Me.InfoToolStripMenuItem.Name = "InfoToolStripMenuItem"
+        Me.InfoToolStripMenuItem.Size = New System.Drawing.Size(40, 20)
+        Me.InfoToolStripMenuItem.Text = "Info"
+        '
+        'VersionToolStripMenuItem
+        '
+        Me.VersionToolStripMenuItem.Name = "VersionToolStripMenuItem"
+        Me.VersionToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.VersionToolStripMenuItem.Text = "Version"
+        '
+        'HerstellerToolStripMenuItem
+        '
+        Me.HerstellerToolStripMenuItem.Name = "HerstellerToolStripMenuItem"
+        Me.HerstellerToolStripMenuItem.Size = New System.Drawing.Size(148, 22)
+        Me.HerstellerToolStripMenuItem.Text = "Hersteller Info"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(274, 562)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(104, 35)
+        Me.Button2.TabIndex = 11
+        Me.Button2.Text = "Beenden"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label3.Location = New System.Drawing.Point(57, 55)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(13, 29)
+        Me.Label3.TabIndex = 16
+        Me.Label3.Text = "."
+        Me.Label3.UseCompatibleTextRendering = True
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label4.Location = New System.Drawing.Point(113, 55)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(13, 29)
+        Me.Label4.TabIndex = 17
+        Me.Label4.Text = "."
+        Me.Label4.UseCompatibleTextRendering = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label5.Location = New System.Drawing.Point(113, 110)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(13, 29)
+        Me.Label5.TabIndex = 19
+        Me.Label5.Text = "."
+        Me.Label5.UseCompatibleTextRendering = True
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label6.Location = New System.Drawing.Point(57, 110)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(13, 29)
+        Me.Label6.TabIndex = 18
+        Me.Label6.Text = "."
+        Me.Label6.UseCompatibleTextRendering = True
+        '
+        'Label7
+        '
+        Me.Label7.AutoSize = True
+        Me.Label7.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label7.Location = New System.Drawing.Point(171, 110)
+        Me.Label7.Name = "Label7"
+        Me.Label7.Size = New System.Drawing.Size(13, 29)
+        Me.Label7.TabIndex = 21
+        Me.Label7.Text = "."
+        Me.Label7.UseCompatibleTextRendering = True
+        '
+        'Label8
+        '
+        Me.Label8.AutoSize = True
+        Me.Label8.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.0!)
+        Me.Label8.Location = New System.Drawing.Point(171, 55)
+        Me.Label8.Name = "Label8"
+        Me.Label8.Size = New System.Drawing.Size(13, 29)
+        Me.Label8.TabIndex = 20
+        Me.Label8.Text = "."
+        Me.Label8.UseCompatibleTextRendering = True
+        '
+        'TreeView1
+        '
+        Me.TreeView1.Location = New System.Drawing.Point(12, 142)
+        Me.TreeView1.Name = "TreeView1"
+        Me.TreeView1.Size = New System.Drawing.Size(366, 378)
+        Me.TreeView1.TabIndex = 22
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(143, 562)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(104, 35)
+        Me.Button3.TabIndex = 23
+        Me.Button3.Text = "Prüfen"
+        Me.Button3.UseVisualStyleBackColor = True
+        Me.Button3.Visible = False
+        '
+        'Label9
+        '
+        Me.Label9.AutoSize = True
+        Me.Label9.Location = New System.Drawing.Point(237, 55)
+        Me.Label9.Name = "Label9"
+        Me.Label9.Size = New System.Drawing.Size(0, 13)
+        Me.Label9.TabIndex = 24
+        '
+        'ProgressBar1
+        '
+        Me.ProgressBar1.Location = New System.Drawing.Point(9, 526)
+        Me.ProgressBar1.Name = "ProgressBar1"
+        Me.ProgressBar1.Size = New System.Drawing.Size(369, 30)
+        Me.ProgressBar1.TabIndex = 25
+        '
+        'Label10
+        '
+        Me.Label10.AutoSize = True
+        Me.Label10.Location = New System.Drawing.Point(237, 55)
+        Me.Label10.Name = "Label10"
+        Me.Label10.Size = New System.Drawing.Size(0, 13)
+        Me.Label10.TabIndex = 26
+        '
+        'Label11
+        '
+        Me.Label11.AutoSize = True
+        Me.Label11.Location = New System.Drawing.Point(237, 110)
+        Me.Label11.Name = "Label11"
+        Me.Label11.Size = New System.Drawing.Size(0, 13)
+        Me.Label11.TabIndex = 27
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(390, 609)
+        Me.Controls.Add(Me.Label11)
+        Me.Controls.Add(Me.Label10)
+        Me.Controls.Add(Me.ProgressBar1)
+        Me.Controls.Add(Me.Label9)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.TreeView1)
+        Me.Controls.Add(Me.Label7)
+        Me.Controls.Add(Me.Label8)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.IPQ5Box)
+        Me.Controls.Add(Me.IPQ6Box)
+        Me.Controls.Add(Me.IPQ7Box)
+        Me.Controls.Add(Me.IPQ8Box)
+        Me.Controls.Add(Me.IPQ4Box)
+        Me.Controls.Add(Me.IPQ3Box)
+        Me.Controls.Add(Me.IPQ2Box)
+        Me.Controls.Add(Me.IPQ1Box)
+        Me.Controls.Add(Me.Button1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximumSize = New System.Drawing.Size(406, 647)
+        Me.MinimumSize = New System.Drawing.Size(406, 647)
+        Me.Name = "Form1"
+        Me.Text = "Netzwerk scanner"
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents IPQ1Box As System.Windows.Forms.TextBox
+    Friend WithEvents IPQ2Box As System.Windows.Forms.TextBox
+    Friend WithEvents IPQ3Box As System.Windows.Forms.TextBox
+    Friend WithEvents IPQ4Box As System.Windows.Forms.TextBox
+    Friend WithEvents IPQ5Box As System.Windows.Forms.TextBox
+    Friend WithEvents IPQ6Box As System.Windows.Forms.TextBox
+    Friend WithEvents IPQ7Box As System.Windows.Forms.TextBox
+    Friend WithEvents IPQ8Box As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents MenuStrip1 As System.Windows.Forms.MenuStrip
+    Friend WithEvents DateiToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ExportierenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents ImportierenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents StartenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents BeendenToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents InfoToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents VersionToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents HerstellerToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Label7 As System.Windows.Forms.Label
+    Friend WithEvents Label8 As System.Windows.Forms.Label
+    Friend WithEvents TreeView1 As System.Windows.Forms.TreeView
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Label9 As System.Windows.Forms.Label
+    Friend WithEvents ProgressBar1 As System.Windows.Forms.ProgressBar
+    Friend WithEvents Label10 As System.Windows.Forms.Label
+    Friend WithEvents Label11 As System.Windows.Forms.Label
+
+End Class
